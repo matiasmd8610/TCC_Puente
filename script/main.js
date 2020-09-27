7df4e690299aa61cdcd9de1579fc5832e809c6b2
Matias Di Iorio <matiasmd8610@gmail.com>
2020-09-27 20:18:57 -0300

    Header + Footer + sección Instalaciones

--- script/main.js
+++ script/main.js
@@ -1,18 +1,29 @@
 $(document).ready(function(){
 
-  //Overlay background when dropdown is open
+  //Overlay background when dropdown is open----------------------
     $("main").on("click", function() {
      if ($(".collapse").hasClass("show")) {
        $('.navbar-collapse').collapse('hide');
      }
     });
 
-    /*$('.collapse').on('hidden.bs.collapse', function() {
-      $('.overlay').fadeOut();
-   });
+    AOS.init({
+      once: true
+    });
 
-   $(".overlay").on("click", function() {
-     $(this).fadeOut('slow');
-   });*/
+    //set class active to current page-----------------------
+    var current = location.pathname;
+    $('#nav ul li a').each(function() {
+        var $this = $(this);
+        // we check comparison between current page and attribute redirection.
+        if ($this.attr('href') === current) {
+            $this.addClass('active');
+        }
+    });
+
+    $('#nav .contact-link').on("click", function() {
+      $('#nav ul li a').removeClass('active');
+      $(this).addClass('active');
+    });
 
 });
